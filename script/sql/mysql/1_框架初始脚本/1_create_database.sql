50e5346a8f3aefff53df7c4190f56945cd09ec19
Ryu <shenjlong@163.com>
2025-04-05 13:12:08 +0800

    删除无用脚本

--- script/sql/mysql/1_框架初始脚本/1_create_database.sql
+++
@@ -1,7 +0,0 @@
--- ----------------------------
--- 强制使用mysql8
--- ----------------------------
-DROP DATABASE IF EXISTS `my-ruoyi-ai`;
-CREATE DATABASE  `my-ruoyi-ai` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
